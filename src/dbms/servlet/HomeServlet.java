d45d467fbac077bac69ca3e82bb1169283d4d9fd
pmcintyre3 <zhdragonx@gmail.com>
2015-07-24 21:07:35 -0400

    Source file for HomeServlet.java

---
+++ src/dbms/servlet/HomeServlet.java
@@ -0,0 +1,52 @@
+package dbms.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.text.DateFormat;
+import java.util.Date;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.json.HTTP;
+import org.json.JSONObject;
+
+import dbms.dao.LoginDao;
+import dbms.dao.OrderDAO;
+import dbms.dao.ProductDao;
+import dbms.dao.UserDAO;
+import dbms.model.Product;
+import dbms.model.User;
+
+/**
+ * Created by Phillip on 7/24/2015.
+ */
+@WebServlet("/HomeServlet")
+public class HomeServlet extends HttpServlet {
+
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public HomeServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+    protected void doGet(HttpServletRequest request,
+                         HttpServletResponse response) throws ServletException, IOException {
+
+        request.setAttribute("productList",ProductDao.getAllProducts(0));
+        RequestDispatcher rd = request.getRequestDispatcher("index.jsp");
+        rd.forward(request, response);
+
+    }
+
+}
